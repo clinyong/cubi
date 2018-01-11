2b897c99207844c30339afc4f44b2f96079752c0
陈林泳 <clinyong@gmail.com>
2018-01-11 13:18:29 +0800

    remove dynamic entry

--- src/startServer.ts
+++ src/startServer.ts
@@ -1,61 +1,10 @@
 import * as webpack from "webpack";
-import { EventEmitter } from "events";
 import * as webpackDevMiddleware from "webpack-dev-middleware";
 import * as express from "express";
 import * as serveStatic from "serve-static";
-import { addWebpackEntry } from "./utils/webpack";
-import { localIP, printWarn } from "./utils";
-
-interface BuiltEntry {
-	[index: string]: {
-		status: symbol;
-	};
-}
+import { localIP } from "./utils";
 
 const host = "0.0.0.0";
-
-const ADDED = Symbol("added");
-const BUILDING = Symbol("building");
-const BUILT = Symbol("built");
-
-class Invalidator {
-	private devMiddleware: any;
-	private building: boolean;
-	private rebuildAgain: boolean;
-
-	constructor(devMiddleware) {
-		this.devMiddleware = devMiddleware;
-		this.building = false;
-		this.rebuildAgain = false;
-	}
-
-	invalidate() {
-		if (this.building) {
-			this.rebuildAgain = true;
-			return;
-		}
-
-		this.building = true;
-		this.devMiddleware.invalidate();
-	}
-
-	startBuilding() {
-		this.building = true;
-	}
-
-	doneBuilding() {
-		this.building = false;
-		if (this.rebuildAgain) {
-			this.rebuildAgain = false;
-			this.invalidate();
-		}
-	}
-}
-
-function getRandomKey(keyList: string[]): string {
-	const randomIndex = Math.floor(Math.random() * keyList.length);
-	return keyList[randomIndex];
-}
 
 export interface ServerStaticPath {
 	prefix: string;
@@ -85,31 +34,6 @@ export function startServer(
 	config: webpack.Configuration,
 	serverConfig: DevServerOption
 ) {
-	const originEntries = config.entry as any;
-	const initEntry = {};
-	const builtEntries: BuiltEntry = {};
-
-	if (serverConfig.initEntry) {
-		serverConfig.initEntry.forEach(k => {
-			if (originEntries[k]) {
-				initEntry[k] = originEntries[k];
-				builtEntries[k] = { status: BUILDING };
-			}
-		});
-	}
-
-	if (Object.keys(initEntry).length > 0) {
-		config.entry = initEntry;
-	} else {
-		const k = getRandomKey(Object.keys(originEntries));
-		config.entry = {
-			[k]: originEntries[k]
-		};
-		builtEntries[k] = {
-			status: BUILDING
-		};
-	}
-
 	const compiler: any = webpack(config);
 	const devMiddleware = webpackDevMiddleware(compiler, {
 		logLevel: "silent",
@@ -119,64 +43,8 @@ export function startServer(
 	});
 
 	const app = express();
-	const doneCallbacks = new EventEmitter();
-	const invalidator = new Invalidator(devMiddleware);
-
-	compiler.plugin("make", function addEntry(compilation, done) {
-		invalidator.startBuilding();
-
-		const allEntries = Object.keys(builtEntries).map(page => {
-			const entry = originEntries[page];
-			builtEntries[page].status = BUILDING;
-			return addWebpackEntry(compilation, this.context, page, entry);
-		});
-
-		Promise.all(allEntries)
-			.then(() => done())
-			.catch(done);
-	});
-	compiler.plugin("done", function emitWebpackDone() {
-		Object.keys(builtEntries).forEach(page => {
-			const entryInfo = builtEntries[page];
-			if (entryInfo.status !== BUILDING) return;
-
-			entryInfo.status = BUILT;
-			doneCallbacks.emit(page);
-			invalidator.doneBuilding();
-		});
-	});
 
 	// init express middleware
-	app.use(function ensurePage(req, res, next) {
-		const requestPath = req.path === "/" ? "/index.html" : req.path;
-		const url: string[] = requestPath.split(".");
-		if (url[1] === "html") {
-			const entryName = url[0].substr(1);
-			if (!originEntries[entryName]) {
-				next();
-				return;
-			}
-
-			const entryInfo = builtEntries[entryName];
-			if (entryInfo) {
-				if (entryInfo.status === BUILT) {
-					next();
-				} else if (entryInfo.status === BUILDING) {
-					doneCallbacks.on(entryName, next);
-				}
-				return;
-			}
-
-			printWarn(`> Building page: ${entryName}`);
-			builtEntries[entryName] = {
-				status: ADDED
-			};
-			doneCallbacks.on(entryName, next);
-			invalidator.invalidate();
-		} else {
-			next();
-		}
-	});
 	app.use(devMiddleware);
 	app.use(
 		require("webpack-hot-middleware")(compiler, {
